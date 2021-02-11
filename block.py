5294227f5390f89dc6951d1764690d0b95ce16d4
RSE77 <rysboards@yahoo.com>
2021-02-11 10:48:05 -0500

    Add existing file

--- block.py
+++ block.py
@@ -1,20 +1,5 @@
 import time
-
-def mine_block(last_block, data):
-    """
-    Mine a block based on the given last_block and data.
-    """
-    timestamp = time.time_ns()
-    last_hash = last_block.hash
-    hash = f'{timestamp}-{last_hash}'
-
-    return Block(timestamp, last_hash, hash, data)
-
-def genesis():
-    """
-    Generate the genisis block.
-    """
-    return Block(1, 'genesis_last_hash', 'genesis_hash', [])
+from crypto_hash import crypto_hash
 
 class Block:
     """
@@ -36,10 +21,30 @@ class Block:
         f'data: {self.data},)'
         )
 
+    @staticmethod
+    def mine_block(last_block, data):
+        """
+        Mine a block based on the given last_block and data.
+        """
+        timestamp = time.time_ns()
+        last_hash = last_block.hash
+        hash = crypto_hash(timestamp, last_hash, data)
+
+        return Block(timestamp, last_hash, hash, data)
+
+    @staticmethod
+    def genesis():
+        """
+        Generate the genisis block.
+        """
+        return Block(1, 'genesis_last_hash', 'genesis_hash', [])
+
 def main():
-    genesis_block = genesis()
-    block = mine_block(genesis_block, 'foo')
+    genesis_block = Block.genesis()
+    block = Block.mine_block(genesis_block, 'foo')
     print(block)
+
+
 
 if __name__ == '__main__':
     main()
